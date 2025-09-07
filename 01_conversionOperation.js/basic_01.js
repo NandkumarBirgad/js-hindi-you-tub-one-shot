293b533dc99a21f63044ddb1108009e9706a60f7
nandkumar  birgad <nandkumarbirgad5@gmail.com>
2025-09-07 16:08:50 +0000

    basic

---
+++ 01_conversionOperation.js/basic_01.js
@@ -0,0 +1,19 @@
+let value = 5;
+console.log("value")
+let str1 = "hello"
+let str2 = " nandu"
+let str3 = str1 + str2
+console.log(str3)
+console.log(1 + 2)
+console.log(1 + '5')
+console.log(1 + '5' + 7)
+console.log("1" + 6 + '7')
+let num1, num2, num3
+num2 = 8
+num3 = 9
+num1 = num2 + num3 + 4 + 5
+console.log(num1)
+
+let gemeCounter = 100
+gemeCounter+
+console.log(gemeCounter);
